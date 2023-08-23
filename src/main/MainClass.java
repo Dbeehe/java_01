3143fb5e334ad5714a6a7fd297c101b9310c1418
Dbeehe <cjh123456a@naver.com>
2023-08-23 16:29:50 +0900

    1

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,5 +1,7 @@
 package main;
 
+import service.StudentService;
+
 import java.util.Scanner;
 
 public class MainClass {
@@ -10,29 +12,40 @@ public class MainClass {
     // 를 선택하는 코드를 작성합니다.
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        StudentService studentService = new StudentService();
         boolean run = true;
 
         while (run) {
             System.out.println("======= 학생관리 =======");
             System.out.println("1.학생등록, 2.학생상세조회, 3.학생목록조회");
             System.out.println("선택> ");
-            int num = Scanner.n();
-            if (num == 1) {
+            int sel = scanner.nextInt();
+            if (sel == 1) {
                 System.out.println("학생등록 메뉴");
+                studentService.save();
                 break;
-            } else if (num == 2) {
+            } else if (sel == 2) {
                 System.out.println("학생조회 메뉴");
+                /*
+                    service 의 findByid 메서드를 호출함.
+                    
+                    service 의 findByid
+                    - id값을 입력받고 repository의 findById로 id값을 넘김
+                    - 입력받은 id에 해당하는 학생 데이터를 리턴받음.
+                    - 리턴받은 값을 출력함
+                 */
                 break;
-            } else if (num == 3) {
+            } else if (sel == 3) {
                 System.out.println("학생목록 메뉴");
+                studentService.findAll();
                 break;
-            } else if (num == 4) {
+            } else if (sel == 4) {
                 System.out.println("학생수정 메뉴");
                 break;
-            } else if (num == 5) {
+            } else if (sel == 5) {
                 System.out.println("학생삭제 메뉴");
                 break;
-            } else if (num == 0) {
+            } else if (sel == 0) {
                 System.out.println("종료");
                 break;
             } else {
--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -1,4 +1,74 @@
 package service;
 
+import dto.StudentDTO;
+import respository.StudentRespository;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class StudentService {
+    Scanner scanner = new Scanner(System.in);
+    StudentRespository studentRespository = new StudentRespository();
+    // 학생등록
+    // 매개변수 없음.
+    // 리턴없음.
+    // 메서드 이름: save()
+    // 실행내용
+    // 스캐너로 이름(studentName), 학과(studentMajor), 전화번호(studentMobile) 를 입력받고
+    // 입력값을 DTO객체에 담아서 StudentRepository의 save() 메서드로 전달
+
+    public void save(){
+        System.out.println("이름> ");
+        String studentName = scanner.next();
+        System.out.println("학과> ");
+        String studentMajor = scanner.next();
+        System.out.println("전화번호> ");
+        String studentMobile = scanner.next();
+        // DTO 객체
+        StudentDTO studentDTO = new StudentDTO();
+        studentDTO.setStudentName(studentName);
+        studentDTO.setStudentMajor(studentMajor);
+        studentDTO.setStudentMobile(studentMobile);
+
+        // DTO 객체를 StudentRespository의 save() 메서드로 전달하여 리턴을 boolean으로 받음.
+        boolean result = studentRespository.save(studentDTO);
+        // respository의 save() 내용
+        // 넘겨받은 DTO 객체를 리스트에 추가하고 추가하면 true 리턴
+        if (result){
+            System.out.println("등록 성공");
+        }else{
+            System.out.println("등록실패");
+        }
+    }
+    public void findAll(){
+        // - Repository의 finAll 메서드를 호출하여 리스트를 리턴받음
+        List<StudentDTO> studentDTOList = studentRespository.findAll();
+        for (StudentDTO studentDTO: studentDTOList){
+            System.out.println("studentDTO = " + studentDTO);
+        }
+    }
+
+        /*
+            findAll() 메서드
+            Main에서 3번 선택시 호출되는 메서드
+            매개변수 리턴 없음.
+            실행내용
+                - Repository의 findAll 메서드를 호출하여 리스트를 리턴받음.
+                - 리턴받은 리스트에 담긴 데이터를 for문을 이용하여 출력
+         */
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
